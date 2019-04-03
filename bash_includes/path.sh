a7f44a0cf7da389e78c110ffdfa550d02857499a
Esther Weidauer <esther@selfawaresoup.com>
2019-04-03 20:52:56 +0200

    path changes

--- bash_includes/path.sh
+++ bash_includes/path.sh
@@ -5,8 +5,7 @@ PATH=/usr/local/lib/ruby/gems/2.6.0/bin:$PATH # homebrew gems
 
 PATH=$PATH:$HOME/.bin # Add home .bin to PATH
 PATH=$PATH:$HOME/.bin/node_modules/.bin
-PATH=$PATH:$HOME/.bin/scala/bin
-PATH=$PATH:$HOME/Library/Haskell/bin
+PATH=$PATH:$HOME/.cargo/bin # rust binaries
 PATH=$PATH:/usr/local/bin # Homebrew bin
 PATH=$PATH:/usr/local/sbin # Homebrew sbi
 PATH=$PATH:/usr/local/share/npm/bin # NPM bins
