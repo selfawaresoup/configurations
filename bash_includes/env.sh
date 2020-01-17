c7a95991573fb7f952448ea655405c8cbebc8e02
Esther Weidauer <esther@selfawaresoup.com>
2020-01-17 19:38:02 +0100

    add catalina setting

--- bash_includes/env.sh
+++ bash_includes/env.sh
@@ -6,3 +6,6 @@ export EDITOR=vim
 
 # ignore case, long prompt, exit if it fits on one screen
 export LESS="-iMFXR"
+
+# silence macOS Catalina default shell notice
+export BASH_SILENCE_DEPRECATION_WARNING=1
