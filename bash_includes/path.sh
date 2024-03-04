ab8efb72169005d1e44d690275fe5e3da0287a7b
Esther Weidauer <esther@selfawaresoup.com>
2024-03-04 15:02:04 +0100

    clean up paths

--- bash_includes/path.sh
+++ bash_includes/path.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-PATH=/usr/local/opt/ruby/bin:$PATH # homebrew ruby
-PATH=/usr/local/lib/ruby/gems/2.6.0/bin:$PATH # homebrew gems
-PATH=/Library/Frameworks/Python.framework/Versions/3.8/bin:$PATH # python3
-
-
+PATH=$PATH:/opt/homebrew/bin # M1 compatible homebrew bin
+PATH=$PATH:/opt/homebrew/sbin # M1 compatible homebrew sbin
 PATH=$PATH:$HOME/.bin # Add home .bin to PATH
 PATH=$PATH:$HOME/.bin/node_modules/.bin
 PATH=$PATH:$HOME/.cargo/bin # rust binaries
-PATH=$PATH:/opt/homebrew/bin # M1 compatible homebrew on macOS
 PATH=$PATH:/usr/local/bin # Homebrew bin
-PATH=$PATH:/usr/local/sbin # Homebrew sbi
+PATH=$PATH:/usr/local/sbin # Homebrew sbin
 PATH=$PATH:/usr/local/share/npm/bin # NPM bins
-
-PATH=$PATH:$HOME/Library/Android/sdk/tools # Android SDK tools
-PATH=$PATH:$HOME/Library/Android/sdk/platform-tools # Android SDK tools
 
